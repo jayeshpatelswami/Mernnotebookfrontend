b91128df9c184da51fe1f088fb311add5110751d
jayeshpatelswami <124601438+jayeshpatelswami@users.noreply.github.com>
2023-05-09 13:48:02 +0530

    Update Login.js

--- src/component/Login.js
+++ src/component/Login.js
@@ -7,7 +7,7 @@ const Login = () => {
   const [information, setinformation] = useState({email:"",password:""})
   const handalsubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("http://localhost:10000/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
