a5c0149e262ace7a097d965a153fc6051a4b8ed4
jayeshpatelswami <124601438+jayeshpatelswami@users.noreply.github.com>
2023-05-09 13:48:22 +0530

    Update Signup.js

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -10,7 +10,7 @@ const Signup = () => {
  const [information, setinformation] = useState({name:"",email:"",password:""})
  const handalsubmit = async (e) => {
    e.preventDefault();
-   const response = await fetch("http://localhost:5000/api/auth/createuser", {
+   const response = await fetch("http://localhost:10000/api/auth/createuser", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",
